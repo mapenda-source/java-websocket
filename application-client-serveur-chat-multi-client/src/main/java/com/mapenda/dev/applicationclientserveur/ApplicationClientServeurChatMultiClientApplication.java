f38bb985c676239d7646f459ac719a18c42270a7
Dioum Mapenda <59739135+mapenda-source@users.noreply.github.com>
2023-12-04 20:27:28 -0500

    first commit

---
+++ application-client-serveur-chat-multi-client/src/main/java/com/mapenda/dev/applicationclientserveur/ApplicationClientServeurChatMultiClientApplication.java
@@ -0,0 +1,13 @@
+package com.mapenda.dev.applicationclientserveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationClientServeurChatMultiClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationClientServeurChatMultiClientApplication.class, args);
+    }
+
+}
---
+++ application-client-serveur-chat-multi-client/src/test/java/com/mapenda/dev/applicationclientserveur/ApplicationClientServeurChatMultiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapenda.dev.applicationclientserveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationClientServeurChatMultiClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
